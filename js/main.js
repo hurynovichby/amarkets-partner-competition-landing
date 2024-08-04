9c22ac1077ccc8522d062693c0b8068963d2dcb1
Andrei_Hurynovich <twain575@gmail.com>
2024-08-04 11:49:07 +0300

    Make header sticky on scroll

--- js/main.js
+++ js/main.js
@@ -1,22 +1,33 @@
 // Скрытие блока countdown
-document.addEventListener('DOMContentLoaded', function () {
-  const countdown = document.querySelector('.countdown');
-  const closeButton = document.querySelector('.countdown__close');
-  
-  const isHidden = localStorage.getItem('countdown-hidden');
+document.addEventListener("DOMContentLoaded", function () {
+  const countdown = document.querySelector(".countdown");
+  const closeButton = document.querySelector(".countdown__close");
+
+  const isHidden = localStorage.getItem("countdown-hidden");
   if (isHidden) {
     const hideTime = parseInt(isHidden, 10);
     const now = new Date().getTime();
     if (now < hideTime) {
-      countdown.style.display = 'none';
+      countdown.style.display = "none";
     } else {
-      localStorage.removeItem('countdown-hidden');
+      localStorage.removeItem("countdown-hidden");
     }
   }
 
-  closeButton.addEventListener('click', function () {
+  closeButton.addEventListener("click", function () {
     const now = new Date().getTime();
-    localStorage.setItem('countdown-hidden', now + 24 * 60 * 60 * 1000);
-    countdown.style.display = 'none';
+    localStorage.setItem("countdown-hidden", now + 24 * 60 * 60 * 1000);
+    countdown.style.display = "none";
+  });
+
+  var header = document.querySelector('.header');
+  var headerOffset = header.offsetTop;
+
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > headerOffset) {
+      header.classList.add('fixed');
+    } else {
+      header.classList.remove('fixed');
+    }
   });
 });
