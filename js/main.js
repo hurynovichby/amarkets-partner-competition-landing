e4de8515294ad2ea6c7690330a69783fa6b26d95
Andrei_Hurynovich <twain575@gmail.com>
2024-08-04 14:09:56 +0300

    add modal window

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Скрытие блока countdown
 document.addEventListener("DOMContentLoaded", function () {
   const countdown = document.querySelector(".countdown");
-  const closeButton = document.querySelector(".countdown__close");
+  const closeButton = document.querySelector(".button__close");
 
   const isHidden = localStorage.getItem("countdown-hidden");
   if (isHidden) {
@@ -20,14 +20,38 @@ document.addEventListener("DOMContentLoaded", function () {
     countdown.style.display = "none";
   });
 
-  var header = document.querySelector('.header');
+  var header = document.querySelector(".header");
   var headerOffset = header.offsetTop;
 
-  window.addEventListener('scroll', function () {
+  window.addEventListener("scroll", function () {
     if (window.scrollY > headerOffset) {
-      header.classList.add('fixed');
+      header.classList.add("fixed");
     } else {
-      header.classList.remove('fixed');
+      header.classList.remove("fixed");
+    }
+  });
+
+  // modal
+  var modalOverlay = document.querySelector(".modal-overlay");
+  var modalClose = document.querySelector(".modal__close");
+  var openModalButton = document.querySelector(".open-modal-button"); // Кнопка для открытия модального окна
+
+  // Открытие модального окна
+  openModalButton.addEventListener("click", function () {
+    modalOverlay.style.display = "flex";
+    document.body.style.overflow = "hidden";
+  });
+
+  // Закрытие модального окна при клике на крестик
+  modalClose.addEventListener("click", function () {
+    modalOverlay.style.display = "none";
+    document.body.style.overflow = "";
+  });
+
+  // Закрытие модального окна при клике вне его
+  modalOverlay.addEventListener("click", function (e) {
+    if (e.target === modalOverlay) {
+      modalOverlay.style.display = "none";
     }
   });
 });
